4ffec40fa90c4dfaf1b786e778d98f45976f99a1
Ryuta Mizutani <r.mizutani@outlook.com>
2019-03-21 08:11:56 +0900

    update190321

--- firmware/BTdials.c
+++ firmware/BTdials.c
@@ -22,30 +22,32 @@ __CONFIG(WRT_OFF & PLLEN_OFF & STVREN_OFF & BORV_19 & LVP_OFF);
 #define ENC4DIR PORTBbits.RB4
 #define ENC5CLK PORTBbits.RB5
 #define ENC5DIR PORTBbits.RB6
-#define ENC0FWD 'W'
-#define ENC0REV 'Q'
-#define ENC1FWD 'S'
-#define ENC1REV 'A'
-#define ENC2FWD 'X'
-#define ENC2REV 'Z'
-#define ENC3FWD 'R'
-#define ENC3REV 'E'
-#define ENC4FWD 'F'
-#define ENC4REV 'D'
-#define ENC5FWD 'V'
-#define ENC5REV 'C'
+#define ENC3FWD 'Q'
+#define ENC3REV 'W'
+#define ENC4FWD 'A'
+#define ENC4REV 'S'
+#define ENC5FWD 'Z'
+#define ENC5REV 'X'
+#define ENC0FWD 'R'
+#define ENC0REV 'E'
+#define ENC1FWD 'F'
+#define ENC1REV 'D'
+#define ENC2FWD 'V'
+#define ENC2REV 'C'
 #define SW1REL 'T'
 #define SW2REL 'G'
 //Bluetooth connection 1:connected 0:none
 #define BLUETOOTH_CONNECTED PORTAbits.RA2
+#define RS232C_CONNECTED PORTCbits.RC5
 #define POWER_SW PUSH_SW1
-#define PUSH_SW1 PORTAbits.RA0
-#define PUSH_SW2 PORTAbits.RA1
-#define MODE_SPP 0x01
-#define MODE_HID 0x02
+#define PUSH_SW2 PORTAbits.RA0
+#define PUSH_SW1 PORTAbits.RA1
+#define MODE_BLUETOOTH 1
+#define MODE_RS232C 2
 
 unsigned int iTimer1;
 unsigned int iPowerSW_Low;
+unsigned char ucMode;
 
 void interrupt isr(void){
 	if(TMR1IF){
@@ -62,18 +64,19 @@ void wait(int icycle) {
 
 #define NINPUT 8
 unsigned char ucEnc[NINPUT][4];
-unsigned char ucTick;
+unsigned int uiTick;
 
 void initMCU(void){
 	unsigned char uc0, uc1;
 
 	OSCCONbits.SPLLEN = 0;//4xPLL disabled
 //	OSCCONbits.SCS = 2;//internal osc
+//	OSCCONbits.IRCF = 0xd;//4 MHz
 	OSCCONbits.IRCF = 0xe;//8 MHz
 	APFCON0bits.RXDTSEL = 1;//RX/DT is on RC5
 	APFCON0bits.TXCKSEL = 1;//TX/CK is on RC4
 
-	ucTick = 0;
+	uiTick = 0;
 	for (uc0=0; uc0<NINPUT; uc0++) {
 		for (uc1=0; uc1<4; uc1++) {ucEnc[uc0][uc1] = 1;}
 	}
@@ -94,10 +97,15 @@ void initMCU(void){
 	GIE=1;		// turn on interrupts
 
 //UART 9600 baud
-	SPBRG = 12;//9600 baud at 8 MHz
 	SPBRGH = 0;
+//9600 baud at 8 MHz
+	SPBRG = 12;
 	TXSTAbits.BRGH = 0;
 	BAUDCONbits.BRG16 = 0;
+//9600 baud at 4 MHz
+//	SPBRG = 103;
+//	TXSTAbits.BRGH = 1;
+//	BAUDCONbits.BRG16 = 1;
 	//Asynchronous
 	TXSTAbits.SYNC = 0;
 	RCSTAbits.SPEN = 1;
@@ -108,16 +116,24 @@ void initMCU(void){
 	ANSELA = 0;//port A to be digital
 	ANSELB = 0;//port B to be digital
 	ANSELC = 0;//port C to be digital
-//	INLVLA = 0;//TTL level inputs
+	INLVLC = 0b11011111;//RC5: TTL level input
 	OPTION_REGbits.nWPUEN = 0;//enable indiv pull-ups
-	WPUA = 0b00011111;//RA4-0: pull up
+	WPUA = 0b00011011;//RA4,3,1,0: pull up; RA2 is hard-wired to pull down
 	WPUB = 0b11110000;//RB7-4: pull up
-	WPUC = 0b11111111;//RC7-0: pull up
+	WPUC = 0b11011111;//RC7,6,4-0: pull up
 
 	TRISA = 0b11011111;//RA5 is output
 	TRISB = 0b11111111;//RB7-0:input;
-	TRISC = 0b11111111;
-	PORTAbits.RA5 = 0;//RN42 on
+	//190315 TRISC = 0b11011111;//RC5/RX for LED driving
+	TRISC = 0b11111111;//RC5/RX for RS232C sense
+
+	if (RS232C_CONNECTED) {//RS232C powered boot
+		ucMode = MODE_RS232C;
+		PORTAbits.RA5 = 1;//RN42 off
+	} else {
+		ucMode = MODE_BLUETOOTH;
+		PORTAbits.RA5 = 0;//RN42 on
+	}
 //	TRISAbits.TRISA2 = 1;//Bluetooth connection on:1 off:0 (RN42 GPIO2)
 	//RC4: UART TX
 	//RC5: UART RX
@@ -131,21 +147,21 @@ void enterSleepMode() {
 	GIE=0;		// turn off all interrupts
 	RCSTAbits.SPEN = 0;//turn off UART
 	TXSTAbits.TXEN = 0;
-//commeted out to reduce sleep current (65 uA ==> 23 uA)
-//	OPTION_REGbits.nWPUEN = 1;//disable indiv RB pull-ups
-//	WPUA = 0x01;//keep the POWER_SW pull up
-//	WPUB = 0x00;
-//	WPUC = 0x00;
+//turn off pull ups to reduce sleep current (220 uA ==> 30-50 uA)
+	WPUA = 0x02;//keep the POWER_SW pull up
+	WPUB = 0x00;
+	WPUC = 0b00;//RC7-0: pull up
+	//190315 PORTCbits.RC5 = 0;//turn off LED
 	TRISA = 0b11111111;
-	ANSELA = 0xfe;//turn off RA4-1 digital inputs
+	ANSELA = 0b11111001;//turn off RA4,3,0 digital inputs
 	ANSELB = 0xff;//port B
-	ANSELC = 0xff;//port C
+	ANSELC = 0b11011111;//port C
 }
 
-unsigned char ucMode;
-
 void initBluetooth() {
-	ucMode = 0;
+	//unsigned char ucLATA5 = LATAbits.LATA5;
+	PORTAbits.RA5 = 0;//force RN42 on
+	//SPP mode only
 	int i;
 	for (int i=0; i<5; i++) {wait(30000);}
 	//$$$
@@ -153,12 +169,6 @@ void initBluetooth() {
 	while(!PIR1bits.TXIF);  TXREG = '$';
 	while(!PIR1bits.TXIF);  TXREG = '$';
 	wait(10000);
-	//SF,1; load factory defaults
-	//while(!PIR1bits.TXIF);  TXREG = 'S';
-	//while(!PIR1bits.TXIF);  TXREG = 'F';
-	//while(!PIR1bits.TXIF);  TXREG = ',';
-	//while(!PIR1bits.TXIF);  TXREG = '1';
-	//while(!PIR1bits.TXIF);  TXREG = 0x0d;
 	//S-,BthDials; device name
 	while(!PIR1bits.TXIF);  TXREG = 'S';
 	while(!PIR1bits.TXIF);  TXREG = '-';
@@ -172,32 +182,21 @@ void initBluetooth() {
 	while(!PIR1bits.TXIF);  TXREG = 's';
 	while(!PIR1bits.TXIF);  TXREG = 0x0d;
 	wait(10000);
-	if (!POWER_SW) {//HID profile
-		ucMode = MODE_HID;
-		//SM,6; pairing mode
-		while(!PIR1bits.TXIF);  TXREG = 'S';
-		while(!PIR1bits.TXIF);  TXREG = 'M';
-		while(!PIR1bits.TXIF);  TXREG = ',';
-		while(!PIR1bits.TXIF);  TXREG = '6';
-		while(!PIR1bits.TXIF);  TXREG = 0x0d;
-		wait(10000);
-		//S~,6; HID
-		while(!PIR1bits.TXIF);  TXREG = 'S';
-		while(!PIR1bits.TXIF);  TXREG = '~';
-		while(!PIR1bits.TXIF);  TXREG = ',';
-		while(!PIR1bits.TXIF);  TXREG = '6';
-		while(!PIR1bits.TXIF);  TXREG = 0x0d;
-		wait(10000);
-		//SH,0203; keyboard, reconnect 4 hosts
-		while(!PIR1bits.TXIF);  TXREG = 'S';
-		while(!PIR1bits.TXIF);  TXREG = 'H';
-		while(!PIR1bits.TXIF);  TXREG = ',';
-		while(!PIR1bits.TXIF);  TXREG = '0';
-		while(!PIR1bits.TXIF);  TXREG = '2';
-		while(!PIR1bits.TXIF);  TXREG = '0';
-		while(!PIR1bits.TXIF);  TXREG = '3';
-		while(!PIR1bits.TXIF);  TXREG = 0x0d;
-		wait(10000);
+	//SM,0; slave mode
+	while(!PIR1bits.TXIF);  TXREG = 'S';
+	while(!PIR1bits.TXIF);  TXREG = 'M';
+	while(!PIR1bits.TXIF);  TXREG = ',';
+	while(!PIR1bits.TXIF);  TXREG = '0';
+	while(!PIR1bits.TXIF);  TXREG = 0x0d;
+	wait(10000);
+	//S~,0; SPP
+	while(!PIR1bits.TXIF);  TXREG = 'S';
+	while(!PIR1bits.TXIF);  TXREG = '~';
+	while(!PIR1bits.TXIF);  TXREG = ',';
+	while(!PIR1bits.TXIF);  TXREG = '0';
+	while(!PIR1bits.TXIF);  TXREG = 0x0d;
+	wait(10000);
+	if (!POWER_SW) {//sniffing off
 		//SW,0000; no sniff
 		while(!PIR1bits.TXIF);  TXREG = 'S';
 		while(!PIR1bits.TXIF);  TXREG = 'W';
@@ -208,22 +207,7 @@ void initBluetooth() {
 		while(!PIR1bits.TXIF);  TXREG = '0';
 		while(!PIR1bits.TXIF);  TXREG = 0x0d;
 		wait(10000);
-	} else {//SPP profile
-		ucMode = MODE_SPP;
-		//SM,0; slave mode
-		while(!PIR1bits.TXIF);  TXREG = 'S';
-		while(!PIR1bits.TXIF);  TXREG = 'M';
-		while(!PIR1bits.TXIF);  TXREG = ',';
-		while(!PIR1bits.TXIF);  TXREG = '0';
-		while(!PIR1bits.TXIF);  TXREG = 0x0d;
-		wait(10000);
-		//S~,0; SPP
-		while(!PIR1bits.TXIF);  TXREG = 'S';
-		while(!PIR1bits.TXIF);  TXREG = '~';
-		while(!PIR1bits.TXIF);  TXREG = ',';
-		while(!PIR1bits.TXIF);  TXREG = '0';
-		while(!PIR1bits.TXIF);  TXREG = 0x0d;
-		wait(10000);
+	} else {//sniffing on
 		//SW,0320; 500 ms sniff
 		while(!PIR1bits.TXIF);  TXREG = 'S';
 		while(!PIR1bits.TXIF);  TXREG = 'W';
@@ -240,23 +224,35 @@ void initBluetooth() {
 	while(!PIR1bits.TXIF);  TXREG = ',';
 	while(!PIR1bits.TXIF);  TXREG = '1';
 	while(!PIR1bits.TXIF);  TXREG = 0x0d;
+
+	//for (int i=0; i<5; i++) {wait(30000);}
+	//PORTAbits.RA5 = ucLATA5;//RN42 resume
 }
 
 void main(void){
 	unsigned char uc0, uc1, uc2, uc3;
 
 	initMCU();
-	initBluetooth();
+	if (ucMode & MODE_BLUETOOTH) initBluetooth();
 	iPowerSW_Low = 0;
 
 	while (1) {
-		ucTick++;
-		uc0 = (ucTick & 0x03);
+		uiTick++;
 
-		//PORTAbits.RA5 = iTimer1 & 0x01;
-		//iPowerSW_Low = 0;
-		//PORTAbits.RA5 = uc0 & 0x01;
+		//LED indicates the BTH connection
+		//uc0 = (uiTick >> 8);
+		//if (ucMode & MODE_USB) PORTCbits.RC5 = 1;
+		//else {
+		//	if (BLUETOOTH_CONNECTED) {
+		//		if ((uc0 & 0xe) == 0x2) PORTCbits.RC5 = 1;
+		//		else PORTCbits.RC5 = 0;
+		//	} else {
+		//		if ((uc0 & 0xa) == 0x2) PORTCbits.RC5 = 1;
+		//		else PORTCbits.RC5 = 0;
+		//	}
+		//}
 
+		uc0 = (uiTick & 0x03);
 		ucEnc[0][uc0] = ENC0CLK;
 		ucEnc[1][uc0] = ENC1CLK;
 		ucEnc[2][uc0] = ENC2CLK;
@@ -265,8 +261,8 @@ void main(void){
 		ucEnc[5][uc0] = ENC5CLK;
 		ucEnc[6][uc0] = PUSH_SW1 ? 0 : 1;//detect switch release
 		ucEnc[7][uc0] = PUSH_SW2 ? 0 : 1;
-		uc1 = ((ucTick & 0x03) + 4 - 1) & 0x03;
-		uc2 = ((ucTick & 0x03) + 4 - 2) & 0x03;
+		uc1 = (uc0 + 4 - 1) & 0x03;
+		uc2 = (uc0 + 4 - 2) & 0x03;
 		for (uc3=0; uc3<NINPUT; uc3++) {
 			if ((ucEnc[uc3][uc2]==1)&&(ucEnc[uc3][uc1]==0)&&(ucEnc[uc3][uc0]==0)) {//detect 'A terminal' = off-on-on
 				switch (uc3) {
@@ -303,13 +299,16 @@ void main(void){
 				}
 			}
 		}
-
-		if (BLUETOOTH_CONNECTED) iTimer1 = 0;
-		if ((iTimer1 > 3000)||(iPowerSW_Low > 6)) {//wait approx 13 min to sleep, or sense continuous SW pressing
-			if (BLUETOOTH_CONNECTED) {
-				if (ucMode & MODE_HID) {
-					while(!PIR1bits.TXIF);  TXREG = 0x00;//send disconnection key
-				} else {
+	
+		if (ucMode & MODE_BLUETOOTH) {
+			if (BLUETOOTH_CONNECTED) iTimer1 = 0;
+			else if (RS232C_CONNECTED) {
+				iTimer1 = 0;
+				ucMode = MODE_RS232C;
+				PORTAbits.RA5 = 1;//RN42 off
+ 			}
+			if ((iTimer1 > 3000)||(iPowerSW_Low > 6)) {//wait approx 13 min to sleep, or sense continuous SW pressing
+				if (BLUETOOTH_CONNECTED) {
 					while(!PIR1bits.TXIF);  TXREG = '$';
 					while(!PIR1bits.TXIF);  TXREG = '$';
 					while(!PIR1bits.TXIF);  TXREG = '$';
@@ -317,15 +316,25 @@ void main(void){
 					while(!PIR1bits.TXIF);  TXREG = 'K';
 					while(!PIR1bits.TXIF);  TXREG = ',';
 					while(!PIR1bits.TXIF);  TXREG = 0x0d;
+					wait(30000);
 				}
-				wait(30000);
+				enterSleepMode();
+				while (POWER_SW == 0) {}//wait for SW released
+				wait(20);
+				//while (POWER_SW) {}//sleep
+				while (POWER_SW && (BLUETOOTH_CONNECTED == 0)) {}//sleep
+				initMCU();
 			}
-			enterSleepMode();
-			while (POWER_SW == 0) {}//wait for SW released
-			wait(20);
-			while (POWER_SW) {}//sleep
-			initMCU();
+		} else {//MODE_RS232C
+			if (RS232C_CONNECTED == 0) {
+				enterSleepMode();
+				//while (POWER_SW == 0) {}//wait for SW released
+				wait(20);
+				while (POWER_SW && (RS232C_CONNECTED == 0)) {}//sleep
+				initMCU();
+			}
 		}
+
 	}
 
 }
